a7930657a6653091ece9939b8daec42a20000804
py-task <2529716798@qq.com>
2018-01-21 11:06:24 +0800

    有点坑

--- base/recommendations.py
+++ base/recommendations.py
@@ -10,3 +10,29 @@ def simDistance(prefs, person1, person2):
     sumOfSquares = sum([pow(prefs[person1][item] - prefs[person2][item], 2) for item in prefs[person1] if item in prefs[person2]])
 
     return 1/(1 + sqrt(sumOfSquares))
+
+def simPearson(prefs, person1, person2):
+    si = {}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item] = 1
+
+    if len(si) == 1:    return 1
+
+    sum1 = sum([prefs[person1][item] for item in si])
+    sum2 = sum([prefs[person2][item] for item in si])
+
+    squareSum1 = pow(sum([prefs[person1][item] for item in si]), 2)
+    squareSum2 = pow(sum([prefs[person2][item] for item in si]), 2)
+
+
+    pearsonSum=sum([prefs[person1][item]*prefs[person2][item] for item in si])
+
+    num = pearsonSum - (sum1 * sum2 / len(si))
+
+
+    den = sqrt(squareSum1 - pow(sum1, 2)/ len(si)) + sqrt(squareSum2 - pow(sum2, 2))
+
+    if den == 0:
+        return 0
+    return num/den
